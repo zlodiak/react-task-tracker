a093b507c2f0a183ca0c7ed33d886a468bfe68a7
kalinin <prozaik81-2@yandex.ru>
2020-11-13 20:28:49 +0300

    display tasks

--- src/components/tasks/Tasks.jsx
+++ src/components/tasks/Tasks.jsx
@@ -1,11 +1,36 @@
 import React, { memo } from 'react';
+import { connect } from 'react-redux';
 
 function Tasks(props) {
+    function renderTaskTable() {
+        return props.tasks.map((task, i) => {
+            return (
+                <>
+                    <div className="line" key={ i }>
+                        <div>id: { task.id }</div>
+                        <div>заголовок: { task.title }</div>
+                        <div>текст: { task.text }</div>
+                        <div>статус: { task.status }</div>
+                        <div>постановщик: { task.userId }</div>
+                        <div>исполнитель: { task.executorId }</div>
+                    </div>
+                    <br/>                
+                </>
+            );
+        });
+    }
+
     return (
         <>
-            Tasks
+            { props.tasks.length && renderTaskTable() }
         </>
     );
 }
 
-export default memo(Tasks);
+const mapStateToProps = state => {
+    return {
+        tasks: state.tasksReducer.tasks,
+    }
+}
+
+export default connect(mapStateToProps, {  })(memo(Tasks));
